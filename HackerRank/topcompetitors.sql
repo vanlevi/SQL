e796d9f5177346385c5bf393210b510b9943092e
vanlevi <30377528+vanlevi@users.noreply.github.com>
2022-01-02 13:39:10 -0500

    Top Competitors - JOIN Clause

--- HackerRank/topcompetitors.sql
+++ HackerRank/topcompetitors.sql
@@ -1,7 +1,7 @@
 /* https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true */
 
 SELECT h.hacker_id, h.name FROM  Hackers AS h
-INNER JOIN Hackers AS h ON h.hacker_id = s.hacker_id
+INNER JOIN Submissions AS s ON h.hacker_id = s.hacker_id
 INNER JOIN Challenges AS c ON c.challenge_id = s.challenge_id
 INNER JOIN Difficulty AS d ON d.difficulty_level = c.difficulty_level
 WHERE s.score = d.score AND c.difficulty_level = d.difficulty_level
