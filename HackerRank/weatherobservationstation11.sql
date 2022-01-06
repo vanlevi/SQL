6500d6264af94fd06fbb36311d675f5f8bc7abaf
vanlevi <30377528+vanlevi@users.noreply.github.com>
2022-01-06 12:29:19 -0500

    SELECT - Weather Observation Station 11

---
+++ HackerRank/weatherobservationstation11.sql
@@ -0,0 +1,5 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true */
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY RLIKE '^[^AEIOUAEIOU].*|.*[^AEIOUaeio]$';
+
